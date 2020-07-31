1fb3df4897cc749527f741a98beab40c55716234
Kai Song GOH (TP) <gohks@TP.EDU.SG>
2020-07-31 11:53:48 +0800

    added code for GovInisider

---
+++ build/lib/minearticles/spiders/govinsider_smartgov.py
@@ -0,0 +1,50 @@
+# -*- coding: utf-8 -*-
+import scrapy
+
+
+class GovinsiderSmartgovSpider(scrapy.Spider):
+    name = 'govinsider_smartgov'
+    allowed_domains = ['govinsider.asia']
+    start_urls = ['https://www.govinsider.asia/smart-gov']
+    next_page = 2
+
+    def parse(self, response):
+        articles = response.xpath("//h2[contains(@class,'entry-title')]")
+        for article in articles:
+            title = article.xpath(".//a/text()").get()
+            link = article.xpath(".//a/@href").get()
+            blurp = article.xpath(".//parent::div/div[contains(@class,'entry-summary')]/p/text()").get()
+            article_url = f"https://govinsider.asia{link}"
+            yield response.follow(url=link, callback=self.parse_article, meta={'article_title': title, 'url': article_url, 'blurp': blurp})
+
+        # get next page, currently stop at 3
+        
+        if self.next_page <= 3:
+            full_url = f"https://govinsider.asia/smart-gov/page/{self.next_page}/"
+            self.next_page += 1
+            yield scrapy.Request(url=full_url, callback = self.parse)
+
+            
+    def parse_article(self,response):
+            title = response.request.meta['article_title']
+            url = response.request.meta['url']
+            blurp = response.request.meta['blurp']
+            img = response.xpath("//div[@class='col-sm-9 post-content-col']/img/@src").get()
+            imgurl = f"https://govinsider.asia{img}"
+
+            paragraphs = response.xpath("//div[@class='entry-content post-content']/p")
+            text =''
+            
+            for para in paragraphs:
+                para_text = para.xpath(".//text()").get()
+                        
+                if para_text is not None:
+                    text = text + para_text
+            
+            yield {
+                'title': title,
+                'blurp' : blurp,
+                'text': text,
+                'url': url,
+                'imgurl' : imgurl
+            }
--- build/lib/minearticles/spiders/infoworldML.py
+++ build/lib/minearticles/spiders/infoworldML.py
@@ -1,34 +1,32 @@
 # -*- coding: utf-8 -*-
 import scrapy
-import logging
 
-class CioSpider(scrapy.Spider):
-    name = 'cio'
-    allowed_domains = ['www.cio.com']
-    start_urls = ['https://www.cio.com/asean/category/analytics']
-    
+
+class InfoworldmlSpider(scrapy.Spider):
+    name = 'infoworldML'
+    allowed_domains = ['www.infoworld.com']
+    start_urls = ['https://www.infoworld.com/category/machine-learning']
 
     def parse(self, response):
-        articles = response.xpath("//div[@class='main-col']/div")
+        articles = response.xpath("//div[@class='post-cont']")
         for article in articles[3:]:
-            title = article.xpath(".//div/h3/a/text()").get()
-            link = article.xpath(".//div/h3/a/@href").get()
-            blurp = article.xpath(".//div/h4/text()").get()
-            article_url = f"https://www.cio.com{link}"            
+            title = article.xpath(".//h3/a/text()").get()
+            link = article.xpath(".//h3/a/@href").get()
+            blurp = article.xpath(".//h4/text()").get()
+            article_url = f"https://www.infoworld.com{link}"
             yield response.follow(url=link, callback=self.parse_article, meta={'article_title': title, 'url': article_url, 'blurp': blurp})
 
         # get next page at the moment, doing 3 hops by specify start=80
         next_page = response.xpath("//a[@id='load-more-index']/@href").get()
-        
         if next_page and next_page != '?start=80':
-            full_url = f"https://www.cio.com/asean/category/analytics/{next_page}"
+            full_url = f"https://www.infoworld.com/category/machine-learning/{next_page}"
             yield scrapy.Request(url=full_url, callback = self.parse)
         
     def parse_article(self,response):
         title = response.request.meta['article_title']
         url = response.request.meta['url']
         blurp = response.request.meta['blurp']
-        paragraphs = response.xpath("//div[@itemprop='articleBody']/p")
+        paragraphs = response.xpath("//section[@class='bodee']/div/p")
         text =''
         for para in paragraphs:
             text = text + para.xpath(".//text()").get()
