1088d14efd39f5564b28ac259cfd03420af94d96
Kai Song GOH (TP) <gohks@TP.EDU.SG>
2020-09-12 15:37:35 +0800

    Added spiders for all categories in OpenGov

---
+++ minearticles/spiders/opengov_education.py
@@ -0,0 +1,57 @@
+# -*- coding: utf-8 -*-
+import scrapy
+import re
+
+class OpengovEducationSpider(scrapy.Spider):
+    name = 'opengov_education'
+    allowed_domains = ['www.opengovasia.com']
+    start_urls = ['http://www.opengovasia.com/education']
+
+        
+    def parse(self, response):
+        
+        articles = response.xpath("//div[@class='elementor-post__text']")
+        otherarticles = response.xpath("//h1[@class='elementor-heading-title elementor-size-default']")
+        
+        for article in articles:
+            title = article.xpath(".//h3/a/text()").get().lstrip()
+            link = article.xpath(".//h3/a/@href").get()
+            link = link.replace("http://","https://")
+            yield response.follow(url=link, callback=self.parse_article, meta={'article_title': title, 'url': link}, dont_filter=True)
+
+        for article in otherarticles:
+            title = article.xpath(".//a/text()").get().lstrip()
+            link = article.xpath(".//a/@href").get()
+            link = link.replace("http://","https://")
+            yield response.follow(url=link, callback=self.parse_article, meta={'article_title': title, 'url': link}, dont_filter=True)
+
+
+    def parse_article(self,response):
+        title = response.request.meta['article_title']
+        url = response.request.meta['url']
+        paragraphs = response.xpath("//div[contains(@class, 'theme-post-content')]/div/p")
+        imgurl = response.xpath("//div[contains(@class, 'theme-post-content')]/preceding-sibling::div[contains(@class,'image')]/div//img/@src").get()
+        article_date = response.xpath("//span[contains(@class,'item--type-date')]/text()").get()
+               
+        regex = re.compile(r'[\n\t]')
+        article_date = regex.sub("", article_date)
+        article_date = article_date.lstrip()
+       
+
+        text =''
+        for para in paragraphs:
+            current = para.xpath(".//text()").get()
+            if current is not None:
+                text = text + current
+        
+        blurp = text.split(".")[:4]
+
+        yield {
+             'category': 'Education',
+             'blurp' : blurp,
+             'imgrul': imgurl,
+             'text': text,
+             'title': title,
+             'url': url,
+             'date': article_date
+         }
